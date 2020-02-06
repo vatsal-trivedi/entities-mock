9490f62681214912294c91f02e6935e9fdc5ac3b
vatsal-trivedi <vatsalt@tradeix.com>
2020-02-06 12:37:31 +0000

    init

---
+++ src/main/kotlin/com/tradeix/entityapimock/EntityApiMockApplication.kt
@@ -0,0 +1,11 @@
+package com.tradeix.entityapimock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EntityApiMockApplication
+
+fun main(args: Array<String>) {
+	runApplication<EntityApiMockApplication>(*args)
+}
---
+++ src/test/kotlin/com/tradeix/entityapimock/EntityApiMockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tradeix.entityapimock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EntityApiMockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
